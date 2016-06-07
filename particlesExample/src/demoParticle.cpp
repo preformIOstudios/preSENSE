6763433ab4074257cdd05c249ee464f865680b69
david allen <mail@ludimotion.com>
2016-06-07 18:48:30 -0400

    More comments and todo's.

--- particlesExample/src/demoParticle.cpp
+++ particlesExample/src/demoParticle.cpp
@@ -18,23 +18,23 @@ void demoParticle::setAttractPoints( vector <ofPoint> * attract ){
 //------------------------------------------------------------------
 void demoParticle::reset(){
 	//the unique val allows us to set properties slightly differently for each particle
-	uniqueVal = ofRandom(-10000, 10000);
+	uniqueVal = ofRandom(-10000, 10000); //TODO: move hard coded values into GUI
 	
-	pos.x = ofRandomWidth();
+	pos.x = ofRandomWidth(); 
 	pos.y = ofRandomHeight();
 	
-	vel.x = ofRandom(-3.9, 3.9);
-	vel.y = ofRandom(-3.9, 3.9);
+	vel.x = ofRandom(-3.9, 3.9); //TODO: move hard coded values into GUI
+	vel.y = ofRandom(-3.9, 3.9); //TODO: move hard coded values into GUI
 	
 	frc   = ofPoint(0,0,0);
 	
-	scale = ofRandom(0.5, 1.0);
+	scale = ofRandom(0.5, 1.0);//TODO: move hard coded values into GUI
 	
 	if( mode == PARTICLE_MODE_NOISE ){
-		drag  = ofRandom(0.97, 0.99);
-		vel.y = fabs(vel.y) * 3.0; //make the particles all be going down
+		drag  = ofRandom(0.97, 0.99);//TODO: move hard coded values into GUI
+		vel.y = fabs(vel.y) * 3.0; //make the particles all be going down //TODO: move hard coded values into GUI
 	}else{
-		drag  = ofRandom(0.95, 0.998);	
+		drag  = ofRandom(0.95, 0.998);	//TODO: move hard coded values into GUI
 	}
 }
 
@@ -49,7 +49,7 @@ void demoParticle::update(){
 		frc.normalize(); //by normalizing we disregard how close the particle is to the attraction point 
 		
 		vel *= drag; //apply drag
-		vel += frc * 0.6; //apply force
+		vel += frc * 0.6; //apply force //TODO: move hard coded values into GUI
 	}
 	else if( mode == PARTICLE_MODE_REPEL ){
 		ofPoint attractPt(ofGetMouseX(), ofGetMouseY());
@@ -60,13 +60,13 @@ void demoParticle::update(){
 		frc.normalize(); 
 		
 		vel *= drag; 
-		if( dist < 150 ){
-			vel += -frc * 0.6; //notice the frc is negative 
+		if( dist < 150 ){//TODO: move hard coded values into GUI
+			vel += -frc * 0.6; //notice the frc is negative //TODO: move hard coded values into GUI
 		}else{
 			//if the particles are not close to us, lets add a little bit of random movement using noise. this is where uniqueVal comes in handy. 			
-			frc.x = ofSignedNoise(uniqueVal, pos.y * 0.01, ofGetElapsedTimef()*0.2);
-			frc.y = ofSignedNoise(uniqueVal, pos.x * 0.01, ofGetElapsedTimef()*0.2);
-			vel += frc * 0.04;
+			frc.x = ofSignedNoise(uniqueVal, pos.y * 0.01, ofGetElapsedTimef()*0.2);//TODO: move hard coded values into GUI
+			frc.y = ofSignedNoise(uniqueVal, pos.x * 0.01, ofGetElapsedTimef()*0.2);//TODO: move hard coded values into GUI
+			vel += frc * 0.04;//TODO: move hard coded values into GUI
 		}
 	}
 	else if( mode == PARTICLE_MODE_NOISE ){
@@ -114,13 +114,13 @@ void demoParticle::update(){
 				vel *= drag;
 				 
 				//lets also limit our attraction to a certain distance and don't apply if 'f' key is pressed
-				if( dist < 300 && dist > 40 && !ofGetKeyPressed('f') ){
-					vel += frc * 0.003;
+				if( dist < 300 && dist > 40 && !ofGetKeyPressed('f') ){//TODO: move hard coded values into GUI
+					vel += frc * 0.003; //TODO: move hard coded values into GUI
 				}else{
 					//if the particles are not close to us, lets add a little bit of random movement using noise. this is where uniqueVal comes in handy. 			
-					frc.x = ofSignedNoise(uniqueVal, pos.y * 0.01, ofGetElapsedTimef()*0.2);
-					frc.y = ofSignedNoise(uniqueVal, pos.x * 0.01, ofGetElapsedTimef()*0.2);
-					vel += frc * 0.4;
+					frc.x = ofSignedNoise(uniqueVal, pos.y * 0.01, ofGetElapsedTimef()*0.2);//TODO: move hard coded values into GUI
+					frc.y = ofSignedNoise(uniqueVal, pos.x * 0.01, ofGetElapsedTimef()*0.2);//TODO: move hard coded values into GUI
+					vel += frc * 0.4;//TODO: move hard coded values into GUI
 				}
 				
 			}
@@ -139,18 +139,18 @@ void demoParticle::update(){
 	//we could also pass in bounds to check - or alternatively do this at the ofApp level
 	if( pos.x > ofGetWidth() ){
 		pos.x = ofGetWidth();
-		vel.x *= -1.0;
+		vel.x *= -1.0;//TODO: move hard coded values into GUI
 	}else if( pos.x < 0 ){
 		pos.x = 0;
-		vel.x *= -1.0;
+		vel.x *= -1.0;//TODO: move hard coded values into GUI
 	}
 	if( pos.y > ofGetHeight() ){
 		pos.y = ofGetHeight();
-		vel.y *= -1.0;
+		vel.y *= -1.0;//TODO: move hard coded values into GUI
 	}
 	else if( pos.y < 0 ){
 		pos.y = 0;
-		vel.y *= -1.0;
+		vel.y *= -1.0;//TODO: move hard coded values into GUI
 	}	
 		
 }
@@ -159,18 +159,18 @@ void demoParticle::update(){
 void demoParticle::draw(){
 
 	if( mode == PARTICLE_MODE_ATTRACT ){
-		ofSetColor(255, 63, 180);
+		ofSetColor(255, 63, 180);//TODO: move hard coded values into GUI
 	}
 	else if( mode == PARTICLE_MODE_REPEL ){
-		ofSetColor(208, 255, 63);
+		ofSetColor(208, 255, 63);//TODO: move hard coded values into GUI
 	}
 	else if( mode == PARTICLE_MODE_NOISE ){
-		ofSetColor(99, 63, 255);
+		ofSetColor(99, 63, 255);//TODO: move hard coded values into GUI
 	}
 	else if( mode == PARTICLE_MODE_NEAREST_POINTS ){
-		ofSetColor(103, 160, 237);
+		ofSetColor(103, 160, 237);//TODO: move hard coded values into GUI
 	}
 			
-	ofDrawCircle(pos.x, pos.y, scale * 4.0);
+	ofDrawCircle(pos.x, pos.y, scale * 4.0);//TODO: move hard coded values into GUI
 }
 
